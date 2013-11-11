6d2057d3b333ae27544f37dbbd6ff92133abfa67
Lars Sjöström <lars.sjostrom@svenskaspel.se>
2013-11-11 10:20:38 +0100

    clean up require

--- lib/flume-logger.rb
+++ lib/flume-logger.rb
@@ -1,9 +1,4 @@
 require 'socket'
-require 'thrift'
-
-#require 'thrift_source_protocol'
-#require 'thrift_flume_event_server'
-
 require 'flume-logger/logger'
 require 'flume-logger/eventserver'
 require 'flume-logger/version'
